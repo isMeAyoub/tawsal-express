71ba00d85303e8ca0f74cfac06c06998908a4849
isMeAyoub <ismeayoub@gmail.com>
2024-03-11 01:10:00 +0000

    api gateway done
    i have done the api gateway check all works as expect but i should work more on code

--- GatewayServer/src/main/java/org/simplon/gateway/GatewayServerApplication.java
+++ GatewayServer/src/main/java/org/simplon/gateway/GatewayServerApplication.java
@@ -1,17 +1,13 @@
-package com.simplon.parametre;
+package org.simplon.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-/**
- * Main Class of ParametreService
- * @Author: Ayoub ait si ahmad
- */
 @SpringBootApplication
 @EnableDiscoveryClient
-public class ParametreServiceApplication {
+public class GatewayServerApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ParametreServiceApplication.class,args);
+        SpringApplication.run(GatewayServerApplication.class,args);
     }
 }
